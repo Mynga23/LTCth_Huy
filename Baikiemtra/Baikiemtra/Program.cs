ba051ca431638ceef1226e4c025c7b35f7cc7f33
DELL <tuongmynga@gmail.com>
2022-04-25 23:03:45 +0700

    First

---
+++ Baikiemtra/Baikiemtra/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Baikiemtra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
