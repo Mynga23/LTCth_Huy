632c3d5ab2510f497b640dddff3a74fec1ba1a91
DELL <tuongmynga@gmail.com>
2022-04-26 08:22:14 +0700

    diemdanh2

---
+++ Diemdanh2/Diemdanh2/animal.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diemdanh2
+{
+   
+    public abstract class animal
+    {
+        public abstract void Eat();
+        protected abstract void AnimalSound();
+    
+
+        public string eat_241
+        {
+            get
+            {
+                return eat_241;
+            }
+
+            set
+            {
+                eat_241 = value;
+            }
+        }
+        public string animalSound_241
+        {
+            get
+            {
+                return animalSound_241;
+            }
+
+            set
+            {
+                animalSound_241 = value;
+            }
+        }
+
+    }
+
+}
