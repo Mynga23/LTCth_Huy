11c5c10bd4f1c5fa423b66d448403ff518bed836
DELL <tuongmynga@gmail.com>
2022-04-26 07:34:42 +0700

    Push

---
+++ BT_Diemdanh/BT_Diemdanh/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BT_Diemdanh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
