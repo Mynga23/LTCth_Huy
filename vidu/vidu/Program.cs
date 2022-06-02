6731ab82e942a170e688701b2c68b5de194e2cda
1911505310241 <1911505310241@sv.ute.udn.vn>
2022-06-02 22:21:33 +0700

    abc

--- vidu/vidu/Program.cs
+++ vidu/vidu/Program.cs
@@ -11,9 +11,7 @@ namespace vidu
         static void Main(string[] args)
         {
             Console.Write("Hello");
+            Console.Write("Hello");
         }
     }
 }
-// cái folder đã up git đâu rồi đâyy, cái file bạn mới tạo này chưa có trên git nên ko up lên đc đâu, ủa bữa h mình thấy là tạo xong rồi up lên
-// tại vì bữa giờ bạn tạo trong cái folder đã up git rồi, nên sẽ up đc, còn cái này file mới, chưa có trên git, nên phải tạo cái responstories mới 
-//không phải nó nawmd trong lth c thày huy luôn rồi hả ?, với lại do mới tải lại vs code nên chưa nhận tài khoản git, để thêm vô lại đã
